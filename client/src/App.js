3ebe89942069d4070a1bb6cd3bf068c9a51ef120
Tucker Leach <leachtucker@gmail.com>
2020-11-06 17:36:58 -0800

    Added private route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import PrivateRoute from "./components/PrivateRoute";
 import Login from "./components/Login";
+import BubblePage from './components/BubblePage';
 import "./styles.scss";
 import "../node_modules/bulma/css/bulma.min.css";
 
 function App() {
+
   return (
     <Router>
       <div className="App">
@@ -13,11 +15,12 @@ function App() {
           <h1 className="title is-1 has-text-white is-half is-inline-block">Bubbles</h1>
         </header>
         <div className="container is-flex is-justify-content-center is-align-items-center mt-6">
-          <Route exact path="/" component={Login} />
+          <Route exact path="/login" component={Login} />
           {/*
             Build a PrivateRoute component that will
             display BubblePage when you're authenticated
           */}
+          <PrivateRoute exact path="/" component={BubblePage}  />
         </div>
       </div>
     </Router>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,26 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { Redirect, useHistory } from "react-router-dom";
+import { postLogin } from '../api/postLogin';
+import { isLogin } from "../utils/isLogin";
+
+const intialFormValues = {
+  username: "",
+  password: ""
+};
 
 const Login = () => {
+  const [formValues, setFormValues] = useState(intialFormValues);
+  const { push } = useHistory();
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  // Runs on form submit
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+
+    const credentials = { username: formValues.username, password: formValues.password };
+    postLogin({ ...credentials })
+      .then(resp => {
+        localStorage.setItem('token', resp.data.payload);
+        push('/');
+      })
+      .catch(err => {
+        console.log(err.response);
+      })
+  };
+
+  // Runs when the inputs change values
+  const onChange = (evt) => {
+    evt.preventDefault();
+    setFormValues({ ...formValues, [evt.target.name]: evt.target.value });
+  };
+
+  // If the user is already logged in, let's push them to the dashboard/home route '/'
+  if (isLogin()) {
+    push('/');
+  };
+
   return (
     <div className="box p-6">
       <h2 className="title is-3">Login</h2>
       <div className="container mx-6">
-        <form>
+        <form onSubmit={onSubmit}>
           <div className="field has-text-left">
-            <label className="label is-inline-block p-0">Email:</label>
+            <label className="label is-inline-block p-0">Username:</label>
             <div className="control has-icons-left has-icons-right">
-              <input className="input" type="text" placeholder="Text input" required />
+              <input className="input" name="username" value={formValues.username} onChange={onChange} type="text" required />
               <span className="icon is-small is-left">
-                <i className="fas fa-envelope"></i>
+                <i className="fas fa-user"></i>
               </span>
             </div>
           </div>
           <div className="field has-text-left mt-1">
             <label className="label p-0">Password:</label>
             <div className="control has-icons-left has-icons-right">
-              <input className="input" type="text" placeholder="Text input" required />
+              <input className="input" name="password" value={formValues.password} onChange={onChange} type="password" required />
               <span className="icon is-small is-left">
                 <i className="fas fa-key"></i>
               </span>
