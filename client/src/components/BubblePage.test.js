7cedc5806e3d31fd6be78469835e48cf172b9afd
Tucker Leach <leachtucker@gmail.com>
2020-11-06 19:11:35 -0800

    Finished testing with jest and mock api

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import BubblePage from "./BubblePage";
 import { getColors as mockGetColors } from '../api/getColors';
 
@@ -14,10 +14,19 @@ test("Fetches data and renders the bubbles", async () => {
     data: [...mockColors]
   });
 
-
-
   const { getByText } = render(<BubblePage />);
 
-  await expect(getByText(/aliceblue/i));
+  await waitFor(() => expect(getByText(/aliceblue/i)));
 
+  await waitFor(() => expect(getByText(mockColors[0].color)));
+  await waitFor(() => expect(getByText(mockColors[1].color)));
+  await waitFor(() => expect(getByText(mockColors[2].color)));
+  await waitFor(() => expect(getByText(mockColors[3].color)));
+  await waitFor(() => expect(getByText(mockColors[4].color)));
+  await waitFor(() => expect(getByText(mockColors[5].color)));
+  await waitFor(() => expect(getByText(mockColors[6].color)));
+  await waitFor(() => expect(getByText(mockColors[7].color)));
+  await waitFor(() => expect(getByText(mockColors[8].color)));
+  await waitFor(() => expect(getByText(mockColors[9].color)));
+  await waitFor(() => expect(getByText(mockColors[10].color)));
 });
