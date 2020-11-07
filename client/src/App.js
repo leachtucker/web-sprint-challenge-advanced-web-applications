f05475fb8c8f749c04043baa0a1013028823630f
Tucker Leach <leachtucker@gmail.com>
2020-11-06 16:59:19 -0800

    Made some general design decisions pertaining to background and layout. Created the AxiosWithAuth util

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,22 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import "../node_modules/bulma/css/bulma.min.css";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <header className="container py-6">
+          <h1 className="title is-1 has-text-white is-half is-inline-block">Bubbles</h1>
+        </header>
+        <div className="container is-flex is-justify-content-center is-align-items-center mt-6">
+          <Route exact path="/" component={Login} />
+          {/*
+            Build a PrivateRoute component that will
+            display BubblePage when you're authenticated
+          */}
+        </div>
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,10 +4,39 @@ const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div className="box p-6">
+      <h2 className="title is-3">Login</h2>
+      <div className="container mx-6">
+        <form>
+          <div className="field has-text-left">
+            <label className="label is-inline-block p-0">Email:</label>
+            <div className="control has-icons-left has-icons-right">
+              <input className="input" type="text" placeholder="Text input" required />
+              <span className="icon is-small is-left">
+                <i className="fas fa-envelope"></i>
+              </span>
+            </div>
+          </div>
+          <div className="field has-text-left mt-1">
+            <label className="label p-0">Password:</label>
+            <div className="control has-icons-left has-icons-right">
+              <input className="input" type="text" placeholder="Text input" required />
+              <span className="icon is-small is-left">
+                <i className="fas fa-key"></i>
+              </span>
+            </div>
+          </div>
+          <div className="field is-grouped container mt-5">
+            <div className="control">
+              <button className="button is-link">Submit</button>
+            </div>
+            <div className="control">
+              <button className="button is-link is-light">Cancel</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
 
